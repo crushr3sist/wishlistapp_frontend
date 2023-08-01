6a3866d846457a75bbbc77f74fd4563693d999ff
Wizock <silent.death3500@gmail.com>
2023-08-02 00:45:22 +1000

    start of css

--- src/auth/register.tsx
+++ src/auth/register.tsx
@@ -1,7 +1,31 @@
-import React from "react";
-
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { Card } from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 const registerComponent = () => {
-  return <>register</>;
+  return (
+    <div style={{ position: "fixed", padding: "50px" }}>
+      <Card
+        style={{
+          // minWidth: "70vw",
+          // minHeight: "85vh",
+          height: "80vh",
+          width: "80vw",
+
+          backgroundColor: "#1111",
+        }}
+      >
+        <CardContent>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <Button variant="contained" style={{}} onClick={() => {}}>
+              Register
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default registerComponent;
